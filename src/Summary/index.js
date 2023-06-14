fa18596115c2cdd50c5cfbecbec579410d02a53c
Jakub Musialowski <mds@mds.biz.pl>
2023-06-14 22:17:06 +0200

    Moved summary conditional information to variables

--- src/Summary/index.js
+++ src/Summary/index.js
@@ -1,7 +1,19 @@
 import "./style.css";
 import { currencies } from "../Currencies";
 
-const Summary = ({convertedAmount, currency}) => {
+const Summary = ({convertedAmount}) => {
+    const selectedCurrencyPrice = `${Object.keys(convertedAmount).length === 0 
+        ? (1 / currencies[0].currencyRate).toFixed(3) 
+        : (+convertedAmount.rate).toFixed(3)}`;
+
+    const selectedCurrency = `${Object.keys(convertedAmount).length === 0 
+        ? (currencies[0].label) 
+        : (convertedAmount.currencyOutLabel)}`;
+
+    const selectedCurrencyRate = `${Object.keys(convertedAmount).length === 0 
+        ? (currencies[0].currencyRate).toFixed(3) 
+        : (+convertedAmount.exchangeRate).toFixed(3)}`;
+    
     return (
         <div className="exchangeForm__items exchangeForm__summary">
             
@@ -20,28 +32,11 @@ const Summary = ({convertedAmount, currency}) => {
                 )}
             </p>
             <p className="exchangeForm__summaryInformation">
-                1 PLN = 
-                <span>
-                    {` ${Object.keys(convertedAmount).length === 0 
-                        ? (1 / currencies[0].currencyRate).toFixed(3) 
-                        : (+convertedAmount.rate).toFixed(3)} 
-                        ${Object.keys(convertedAmount).length === 0 
-                        ? (currencies[0].label) 
-                        : (convertedAmount.currencyOutLabel)} `
-                    }
-                </span>
-                według średniego kursu NBP 
-                <span>
-                    {` ${Object.keys(convertedAmount).length === 0 
-                        ? (currencies[0].currencyRate).toFixed(3) 
-                        : (+convertedAmount.exchangeRate).toFixed(3)} `
-                    }
-                </span> 
+                1 PLN = {selectedCurrencyPrice} {selectedCurrency}{" "}
+                według średniego kursu NBP {selectedCurrencyRate}{" "}
                 PLN z dnia 19.08.2022r.</p>
         </div>
     )
 };
 
 export default Summary;
-
-// Object.keys(convertedAmount).length === 0
